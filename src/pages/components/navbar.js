532b12be8df7c71a1d88506765318d3a860368ca
Kristel Fung <kristelfung@berkeley.edu>
2023-06-30 17:31:36 +0800

    navbar hover animation done

--- src/pages/components/navbar.js
+++ src/pages/components/navbar.js
@@ -1,22 +1,48 @@
-import * as React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
+import { useLocation } from '@reach/router'
 
 const Navbar = () => {
+  // Used to assign 'is-active' to current page
+  const location = useLocation();
+  const currentPath = location.pathname;
+
+  // Use state to store the active link
+  const [activeLink, setActiveLink] = useState("")
+
+  // Function to handle link hover
+  const handleLinkHover = (event) => {
+    setActiveLink(event.target)
+  }
+  console.log(activeLink);
+
   return (
     <div className="hero-head">
       <nav className="navbar">
         <div className="container is-max-widescreen">
           <div className="navbar-menu">
             <div className="navbar-end">
-              <Link className="navbar-item" to="/">
+              <Link className={`navbar-item ${currentPath === '/' && activeLink == '' ?  'is-active' : ''}`} 
+                to="/"
+                onMouseEnter={handleLinkHover} // Call handleLinkHover function on mouse enter
+                onMouseLeave={() => setActiveLink("")} // Clear active link when mouse leaves
+              >
                 home
                 <span className="navbar-item-hover"></span>
               </Link>
-              <Link className="navbar-item" to="/about">
+              <Link className={`navbar-item ${currentPath === '/about/' && activeLink == '' ? 'is-active' : ''}`}  
+                to="/about"
+                onMouseEnter={handleLinkHover} // Call handleLinkHover function on mouse enter
+                onMouseLeave={() => setActiveLink("")} // Clear active link when mouse leaves
+              >
                 about
                 <span className="navbar-item-hover"></span>
               </Link>
-              <Link className="navbar-item" to="/blog">
+              <Link className={`navbar-item ${currentPath === '/blog/' && activeLink == '' ? 'is-active' : ''}`}  
+                to="/blog"
+                onMouseEnter={handleLinkHover} // Call handleLinkHover function on mouse enter
+                onMouseLeave={() => setActiveLink("")} // Clear active link when mouse leaves
+              >
                 blog
                 <span className="navbar-item-hover"></span>
               </Link>
