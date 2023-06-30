afaf5c46487bcc550d3978935f64eb48440d2c17
Kristel Fung <kristelfung@berkeley.edu>
2023-06-30 17:46:07 +0800

    use regex for /blog/ to match all /blog/*

--- src/pages/components/navbar.js
+++ src/pages/components/navbar.js
@@ -15,13 +15,16 @@ const Navbar = () => {
     setActiveLink(event.target)
   }
 
+  // Regex for blog path
+  const blogPathRegex = /^\/blog\/.*/
+
   return (
     <div className="hero-head">
       <nav className="navbar">
         <div className="container is-max-widescreen">
           <div className="navbar-menu">
             <div className="navbar-end">
-              <Link className={`navbar-item ${currentPath === '/' && activeLink == '' ?  'is-active' : ''}`} 
+              <Link className={`navbar-item ${currentPath === '/' && activeLink === '' ?  'is-active' : ''}`} 
                 to="/"
                 onMouseEnter={handleLinkHover} // Call handleLinkHover function on mouse enter
                 onMouseLeave={() => setActiveLink("")} // Clear active link when mouse leaves
@@ -29,7 +32,7 @@ const Navbar = () => {
                 home
                 <span className="navbar-item-hover"></span>
               </Link>
-              <Link className={`navbar-item ${currentPath === '/about/' && activeLink == '' ? 'is-active' : ''}`}  
+              <Link className={`navbar-item ${currentPath === '/about/' && activeLink === '' ? 'is-active' : ''}`}  
                 to="/about"
                 onMouseEnter={handleLinkHover} // Call handleLinkHover function on mouse enter
                 onMouseLeave={() => setActiveLink("")} // Clear active link when mouse leaves
@@ -37,7 +40,7 @@ const Navbar = () => {
                 about
                 <span className="navbar-item-hover"></span>
               </Link>
-              <Link className={`navbar-item ${currentPath === '/blog/' && activeLink == '' ? 'is-active' : ''}`}  
+              <Link className={`navbar-item ${blogPathRegex.test(currentPath) && activeLink === '' ? 'is-active' : ''}`}  
                 to="/blog"
                 onMouseEnter={handleLinkHover} // Call handleLinkHover function on mouse enter
                 onMouseLeave={() => setActiveLink("")} // Clear active link when mouse leaves
