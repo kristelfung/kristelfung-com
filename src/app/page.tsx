fed0185a32796acceb491298cebac3ca70ac9c57
Kristel Fung <kristelfung@berkeley.edu>
2024-12-29 15:49:32 +0800

    fix home img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,13 @@ export default function Home() {
           <span className="block py-2">hi! i&apos;m kristel,</span>
           <span className="block py-2">a software engineer based in nyc.</span>
         </h1>
-        <Image src="/home.png" alt="homepage-image" width={1920} height={316} />
+        <Image
+          src="/home.png"
+          alt="homepage-image"
+          width={1063}
+          height={308}
+          className="ml-auto w-[600px]"
+        />
       </div>
       <Footer />
     </FadeIn>
